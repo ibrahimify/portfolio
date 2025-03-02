d935c2edd1146c9afd7fd80c77c361969a1376df
Muhammad Ibrahim Shoeb <muhammadibrahimshoeb@gmail.com>
2025-03-02 04:28:55 +0100

    Fix issue with Android view

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
   mode: "jit",
   theme: {
     extend: {
@@ -19,9 +19,9 @@ module.exports = {
         xs: "450px",
       },
       backgroundImage: {
-        "hero-pattern": "url('/src/assets/herobg.png')",
+        "hero-pattern": "url('./assets/herobg.png')",
       },
-    },
+          },
   },
   plugins: [],
 };
