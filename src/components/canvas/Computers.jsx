ac0e2b4a7faa5d0a0ab9900336478d5fb87af99e
Muhammad Ibrahim Shoeb <muhammadibrahimshoeb@gmail.com>
2025-03-27 15:16:05 +0100

    Updated Project Details

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -38,7 +38,7 @@ const ComputersCanvas = () => {
 
     // Set the initial value of the `isMobile` state variable
     setIsMobile(mediaQuery.matches);
-
+    //the flex name should be divided into the funtions
     // Define a callback function to handle changes to the media query
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches);
